7311fbd17f7c2d7c3520e9b8368771a37b48146d
Fel <99955631+EffelZefanya@users.noreply.github.com>
2023-07-25 17:15:40 +0700

    feat:add Eloquent relation and update model for article, category, user

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "name"
+    ];
+
+    public function article(): HasMany
+    {
+        return $this->HasMany(Article::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
